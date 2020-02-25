c871f1be997d3238f55c73052d8732ebcfb98cba
lucien <lr112@hw.ac.uk>
2020-02-25 15:33:59 +0000

    pathfinding and component creation

---
+++ component/pathfinding.hpp
@@ -0,0 +1,19 @@
+//
+// Created by lucienR on 15/02/2020.
+//
+#include <iostream>
+using namespace std
+
+#ifndef GAMEDESIGN_PATHFINDING_HPP
+#define GAMEDESIGN_PATHFINDING_HPP
+
+#endif //GAMEDESIGN_PATHFINDING_HPP
+
+class pathfinding{
+public:
+    void map();
+
+    void heuristics();
+
+    void Astar();
+};
